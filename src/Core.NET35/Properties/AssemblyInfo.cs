1dec5a9e375d125588948a450668ec867439d337
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-10-13 15:45:13 +0200

    Fixed NET35 projects and assembly information (Data.HashFunction.Core / Data.HashFunction.Interfaces)

--- src/Core.NET35/Properties/AssemblyInfo.cs
+++ src/Core.NET35/Properties/AssemblyInfo.cs
@@ -2,35 +2,13 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Data.HashFunction.Core.NET35")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data.HashFunction.Core.NET35")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Data.HashFunction.Core")]
+[assembly: AssemblyProduct("System.Data.HashFunction.Core")]
+[assembly: AssemblyDescription("C# library to create a common interface to non-cryptographic hash functions (http://en.wikipedia.org/wiki/List_of_hash_functions#Non-cryptographic_hash_functions) and provide implementations of public hash functions.  Includes wrapper for HashAlgorithm-based hash functions.")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("555d81db-1385-41c6-974d-5ab4d22cdc0f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#if CUSTOM_VERSIONING
+[assembly: AssemblyVersion("0.0.0.0")]
+#endif
