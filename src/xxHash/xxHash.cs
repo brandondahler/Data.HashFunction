148887fce95de8264c3fe2a648643922e9600274
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-10-13 15:17:02 +0200

    Removed unused namespaces from Data.HashFunction.xxHash sources

--- src/xxHash/xxHash.cs
+++ src/xxHash/xxHash.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.HashFunction.Utilities;
+﻿using System.Collections.Generic;
 using System.Data.HashFunction.Utilities.IntegerManipulation;
 using System.Data.HashFunction.Utilities.UnifiedData;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace System.Data.HashFunction
